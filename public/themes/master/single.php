48149818f6950f628dc0ede87bb6ab6fd7980e8b
WebShum <webshumweb@gmail.com>
2024-06-12 18:47:24 +0300

    add single

---
+++ public/themes/master/single.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main class="main-page">
+    <div class="center">
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+
+        <?php
+        if ( comments_open() || get_comments_number() ) {
+            comments_template();
+        }
+        ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
